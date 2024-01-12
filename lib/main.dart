d9f876652a7f21d51e3de1ea82ef2854c07b343a
Aayush Chaube <chaubeaayush@gmail.com>
2024-01-12 22:52:46 +0530

    Removed debug banner and optimized _questionBank property of QuizBrain class.

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: const SafeArea(
